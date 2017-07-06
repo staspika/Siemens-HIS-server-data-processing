6f1f858fe27128e5e2d21ce675d0089bec9696a4
Stanislav Pika <staspika@gmail.com>
2017-07-06 10:22:27 +0200

    Several improvements to make the script clearer.

--- init.py
+++ init.py
@@ -1,115 +1,68 @@
 #!/usr/bin/env python3
+# -*- encoding: utf-8-*-
 
+from __future__ import print_function, unicode_literals, division
 from pylab import *
-from datetime import datetime, timedelta
-from pathlib import PurePath
-import dateutil
-import csv
-import json
-import glob
+import pandas
 import itertools
-import re
-import os
 import seaborn
-import json
+import easygui
+import configparser
+import sys
 
-seaborn.set_style("white")
-seaborn.set_style("ticks")
+print(sys.version)
+config = configparser.ConfigParser()
+config.read("config.ini")
+dir_src_data = config['Paths']['DirSourceData'].strip('"')
+dir_images = config['Paths']['DirImages'].strip('"')
+seaborn.set(color_codes=True)
+seaborn.set_style('whitegrid')
+seaborn.set_style('ticks')
 
-with open('config.json') as config_file:
-    gconf = json.load(config_file)
-
-path_root = PurePath(gconf['dir.root'])
-path_srcdata = PurePath(gconf['dir.data'])
-path_images = PurePath(gconf['dir.images'])
-csv.register_dialect('his', delimiter = ',', skipinitialspace = True)
-his_timestamp_format = '%m/%d/%Y %H:%M:%S'
-
-def readTimestamp(timestamp):
-    k = timestamp.find('*')
-    if k >= 0:
-        if timestamp[k-1] == ' ':
-            timeshift = -1
-        elif timestamp[k+1] == ':':
-            timeshift = +1
-        timestamp = ''.join(timestamp.split('*'))
-    ts = datetime.strptime(timestamp, his_timestamp_format)
-    return ts
-
-def readRawData(filename):
-    w = []
-    with open(filename, 'r', newline='') as csvfile:
-        reader = csv.DictReader(csvfile, dialect='his')
-        for row in reader:
-            timestamp = readTimestamp(row['Time stamp']) + timedelta(seconds=0.001*int(row['Milliseconds']))
-            w.append({'Path': row['Point Name'],
-                      'Timestamp': timestamp,
-                      'Value': float(row['Value']),
-                      'Status': int(row['Status']),
-                      'Source / Quality': row['Source / Quality'],
-                      'Measurement units': row['Measurement Units']})
-    return w
-
-def diff(n):
-    m = []
-    for i in range(1, len(n)):
-        k = n[i]-n[i-1]
-        m.append(k)
-    return m
-
-data_files = glob.iglob(path_srcdata.joinpath("*.csv").as_posix())
-t_start = dateutil.parser.parse('2016-01-01T00:00')
-t_stop = dateutil.parser.parse('2017-01-01T00:00')
-print('For the selected period ({0} – {1}):'.format(t_start, t_stop))
-for src_file in data_files:
-    name = os.path.basename(src_file)
-    w = readRawData(src_file)
-    with open("./data/{0}.json".format(name), "w") as json_file:
-        json.dump(w, json_file, default=str)
-    timeline = [x['Timestamp'] for x in w]
-    stat = [x['Status'] for x in w]
-    mask = [(t>=t_start) and (t<=t_stop) for t in timeline]
-    print('{0}: {1} records'.format(name, mask.count(True)))
-    if mask.count(True) > 1: # If there are data for the selected period, make graphs
-        timeline = list(itertools.compress(timeline, mask))
-        stat = list(itertools.compress(stat, mask))
-        fig0 = figure()
-        plot(timeline, stat, drawstyle='steps-post')
-        ylim([-2.2, 1.2])
-        xlabel('Time')
-        ylabel('Status')
-        yticks([-2, -1, 0, 1])
-        title(name)
-        fig0.savefig(path_images.joinpath(name + '.state.png').as_posix())
-        mask = [n==0 or n==1 for n in stat]
-        timeline = list(itertools.compress(timeline, mask))
-        stat = list(itertools.compress(stat, mask))
-        close('all')
-        # Calculate disconnections
-        # A disconnection period is a time difference between first zero after a one and a first one after a zero
-        mask = full(len(stat), True)
-        for i in range(1, len(stat)):
-            if stat[i] == stat[i-1]:
-                mask[i] == False
-        if mask.all() != True:
-            print("Duplicate values in state array!")
-            timeline = list(itertools.compress(timeline, mask))
-            stat = list(itertools.compress(stat, mask))
-        dt = diff(timeline)
-        ds = diff(stat)
-        mask = [n>0 for n in ds]
-        disconnections = [n for n in list(itertools.compress(dt, mask))]
-        disc_sec = [n.total_seconds() for n in disconnections]
-        gDisc = disconnections
-        gStat = stat
-        gTime = timeline
-        fig1 = figure()
-        #~ seaborn.distplot(disc_sec, rug=True, kde=False)
-        hist(disc_sec, bins=logspace(-1, 7, 9, endpoint=True))
-        #~ vlines(disc_sec, 0, 5)
-        xscale('log')
-        xlabel('Duration of disconnection, s')
-        ylabel('Number of disconnections')
-        title(src_file)
-        savefig(path_images.joinpath(name + '.hist.png').as_posix())
-        close('all')
+# Read Siemens HIS file
+filenames = easygui.fileopenbox(default=dir_src_data+"/*.csv", multiple=True)
+for filename in filenames:
+    sitename = filename.split('.')[0]
+    X = pandas.read_csv(filename, skipinitialspace=True, comment="#")
+    # Convert time stamp to a nicer format
+    X['Time stamp'] = pandas.to_datetime(X['Time stamp'], infer_datetime_format=True)
+    X['Milliseconds'] = pandas.to_timedelta(X['Milliseconds'], unit='ms')
+    X['Time stamp'] = X['Time stamp'] + X['Milliseconds']
+    X = X.drop('Milliseconds', axis=1)
+    X = X.sort_values('Time stamp')
+    for pointname in set(X['Point Name']):
+        mask = (X['Point Name'] == pointname)
+        timeline = X[mask]['Time stamp']
+        stat = X[mask]['Status']
+        # Plot state
+        with seaborn.axes_style('whitegrid'):
+            figure()
+            plot(timeline, stat, drawstyle='steps-post')
+            seaborn.despine()
+            xlabel('Time')
+            ylabel('Status')
+            yticks([1, 0, -1, -2], ['In', 'Out', 'Fault', 'Intermediate'])
+            axis('tight')
+            title(pointname)
+            savefig("{0}/{1}".format(dir_images, pointname.replace('/', '-')+'-01-status.png'))
+            close()
+        # Plot statistics
+        with seaborn.axes_style('whitegrid'):
+            figure()
+            mask = [n==0 or n==1 for n in stat]
+            stat = stat[mask]
+            timeline=timeline[mask]
+            dt = diff(timeline)
+            ds = diff(stat)
+            mask = [n>0 for n in ds]
+            disconnections = [n for n in list(itertools.compress(dt, mask))]
+            disc_sec = disconnections / timedelta64(1, 's')
+            distr = logspace(-1, 7, 9, endpoint=True)
+            seaborn.distplot(disc_sec, bins=distr, hist=True, kde=False, rug=True)
+            seaborn.despine()
+            xscale('log')
+            xlabel('Duration of disconnection, s')
+            ylabel('Number of disconnections (total {0})'.format(len(disconnections)))
+            title(pointname)
+            savefig("{0}/{1}".format(dir_images, pointname.replace('/', '-')+'-02-histogram.png'))
+            close()
